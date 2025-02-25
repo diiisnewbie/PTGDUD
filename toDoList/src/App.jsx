8b7044079be9c724085c67bcb9e0429802954cc6
diiisnewbie <nhatduy20072004@gmail.com>
2025-02-25 16:09:08 +0700

    done

---
+++ toDoList/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import StudentApp from "./components/StudentApp";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Quản lý Sinh viên</h1>
+      <StudentApp />
+    </div>
+  );
+}
+
+export default App;
